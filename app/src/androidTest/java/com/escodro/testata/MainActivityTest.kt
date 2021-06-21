00964a34b4b23f5b260dd193a82a5791974b252c
Igor Escodro <escodro@outlook.com>
2021-06-21 13:50:31 -0300

    Initial commit

---
+++ app/src/androidTest/java/com/escodro/testata/MainActivityTest.kt
@@ -0,0 +1,18 @@
+package com.escodro.testata
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.escodro.testata", appContext.packageName)
+    }
+}
